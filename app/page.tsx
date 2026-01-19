2c1cb12fe8686572be8abc2a2746e18e9cc0b022
Lucas <lucas9godoy@gmail.com>
2026-01-19 16:40:44 -0300

    as

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import { TaskForm } from '@/components/task-form';
 
 export default function Home() {
   return (
-    <div className='bg-zinc-900 min-h-screen text-white flex items-center justify-center p-4'>
-      {/* Contenedor que limita ancho y permite scroll interno si el form es muy grande */}
-      <div className='w-full max-w-sm flex flex-col gap-4 h-full max-h-full'>
+    <div className='bg-zinc-900 min-h-[100dvh] text-white flex items-center justify-center p-4'>
+      <div className='w-full max-w-sm flex flex-col gap-4 max-h-[100dvh]'>
         <TaskForm />
       </div>
     </div>
