6fde7842c7f6a652d736324afcea7417af4f4abd
Lucas <lucas9godoy@gmail.com>
2026-01-19 16:36:54 -0300

    as

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import { TaskForm } from '@/components/task-form';
 
 export default function Home() {
   return (
-    <div className='bg-zinc-900 h-screen text-white flex items-center justify-center max-h-screen overflow-y-auto'>
-      <TaskForm />
+    <div className='bg-zinc-900 min-h-screen text-white flex items-center justify-center p-4'>
+      {/* Contenedor que limita ancho y permite scroll interno si el form es muy grande */}
+      <div className='w-full max-w-sm flex flex-col gap-4 h-full max-h-full'>
+        <TaskForm />
+      </div>
     </div>
   );
 }
