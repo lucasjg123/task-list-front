18f0d9c9aa0fc38354449fe4ab3317657d05d575
Lucas <lucas9godoy@gmail.com>
2026-01-19 16:13:34 -0300

    asdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { TaskForm } from '@/components/task-form';
-import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div className='bg-zinc-900 h-screen text-white flex items-center justify-center'>
-      <Button>hola</Button>
+      <TaskForm />
     </div>
   );
 }
